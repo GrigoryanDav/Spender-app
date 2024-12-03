732e71b5576395eb4c4a0e926f758a6416a6e3b4
GrigoryanDav <grigoryand219@gmail.com>
2024-12-03 20:03:09 +0400

    Implement user creating functionality on register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,32 @@
 import { Form, Input, Button } from "antd"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { ROUTES } from "../../constants/routes"
+import { useState } from "react"
+import { auth } from "../../services/firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { RegisterFormValues } from "../../ts/interfaces/RegisterFormValues"
 
 const Register = () => {
+    const [loading, setLoading] = useState(false)
+    const [form] = Form.useForm()
+    const navigate = useNavigate()
+
+    const handleRegister = async (values: RegisterFormValues) => {
+        setLoading(true)
+        const { email, password } = values
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+            navigate(ROUTES.LOGIN)
+        } catch (error) {
+            console.log('Error:', error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
         <div>
-            <Form layout="vertical">
+            <Form layout="vertical" form={form} onFinish={handleRegister}>
                 <Form.Item
                     label='First Name'
                     name='firstName'
@@ -58,7 +79,7 @@ const Register = () => {
                     <Input.Password placeholder="Password"/>
                 </Form.Item>
 
-                <Button type="primary">Sign Up</Button>
+                <Button type="primary" htmlType="submit" loading={loading}>Sign Up</Button>
                 <Link to={ROUTES.LOGIN}><Button>Sign In</Button></Link>
             </Form>
         </div>
