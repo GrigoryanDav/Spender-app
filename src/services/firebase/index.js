65ffbdc731190f82a25490f2143aad47bc82c98f
GrigoryanDav <grigoryand219@gmail.com>
2024-12-03 19:05:03 +0400

    Add firebase auth config

---
+++ src/services/firebase/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "spender-app-1d257.firebaseapp.com",
+  projectId: "spender-app-1d257",
+  storageBucket: "spender-app-1d257.firebasestorage.app",
+  messagingSenderId: "879540832980",
+  appId: "1:879540832980:web:d698f2fe12aaa0cfccc941"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export {
+    auth,
+}
