26dc858f53fae20d504b8845971a8a2df51c03bf
GrigoryanDav <grigoryand219@gmail.com>
2024-12-06 18:16:39 +0400

    Add styles and change structure for ExpenseTypeData page

--- src/pages/ExpenseTypeData/index.tsx
+++ src/pages/ExpenseTypeData/index.tsx
@@ -1,14 +1,17 @@
 import { fetchExpenses } from "../../state-managment/slices/expenses"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../ts/interfaces/rootState"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 import { useEffect } from "react"
 import { AppDispatch } from "../../state-managment/store"
+import { Button } from "antd"
+import { ROUTES } from "../../constants/routes"
+import './index.css'
 
 const ExpenseTypeData = () => {
     const dispatch = useDispatch<AppDispatch>()
     const { expenseType } = useParams()
-    const { expenses, loading, error } = useSelector((store: RootState) => store.expenses)
+    const { expenses } = useSelector((store: RootState) => store.expenses)
     const { authUserInfo: { userData } } = useSelector((store: RootState) => store.userProfile)
 
     useEffect(() => {
@@ -16,21 +19,30 @@ const ExpenseTypeData = () => {
             dispatch(fetchExpenses({ uid: userData.uid, expenseType: expenseType }))
         }
     }, [dispatch, expenseType, userData])
-    
+
     return (
         <div className="expenses-page">
             <h2>Your Expenses</h2>
             {expenses.length > 0 ? (
-                <ul>
-                    {expenses.map((expense, index) => (
-                        <li key={index}>
-                            <p>Amount: {expense.amount}</p>
-                            <p>Description: {expense.description}</p>
-                            <p>Type: {expense.type}</p>
-                            <p>Date: {new Date(expense.createdAt).toLocaleString()}</p>
-                        </li>
-                    ))}
-                </ul>
+                <div className="data_container">
+                    <h3>{expenses[0].type}</h3>
+                    <ul>
+                        {expenses.map((expense, index) => (
+                            <li key={index}>
+                                <div>
+                                    <div className="amount_description_container">
+                                        <p className="data_amount">Amount: {expense.amount}</p>
+                                        <p className="data_description">Description: {expense.description}</p>
+                                    </div>
+                                    <p className="data_time">Date: {new Date(expense.createdAt).toLocaleString()}</p>
+                                </div>
+
+
+                            </li>
+                        ))}
+                    </ul>
+                    <Link to={ROUTES.CABINET}><Button>Cabinet</Button></Link>
+                </div>
             ) : (
                 <p>No expenses found.</p>
             )}
