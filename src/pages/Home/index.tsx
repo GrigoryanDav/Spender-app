f8e85de5d0058ba30f9a79ac103306212b762252
GrigoryanDav <grigoryand219@gmail.com>
2024-12-08 22:29:20 +0400

    Add styles and html structure for Home and add config for images

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Button } from "antd"
 import { ROUTES } from "../../constants/routes"
 import { Link } from "react-router-dom"
+import BackgroundImage from '../../images/home-background.jpg'
 import './index.css'
 
 const Home = () => {
     return (
-        <div className="home_container">
-            <h2></h2>
+        <div className="home_container" style={{background: `url(${BackgroundImage})`}}>
+            <h2>your finances in one place.</h2>
+            <p>Flexibly budget and track your spending, so you stay in control and achieve your goals.</p>
             <Link to={ROUTES.REGISTER}><Button type="primary">Get Started</Button></Link>
         </div>
     )
